63663d99bbcdb6838f2ee7d02654cf7e20a66ad4
ibrown <ibrown@twitter.com>
2018-07-12 17:38:00 -0700

    adding a few unit tests

--- src/levenshtein.erl
+++ src/levenshtein.erl
@@ -1,6 +1,12 @@
 -module(levenshtein).
 -export([levenshtein_distance/2]).
 
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+
 levenshtein_distance(Source, Target)  ->
     Score=lists:append([[X || X <- lists:seq(0, length(Target))]],   [lists:append([Y],[n || X <- lists:seq(1, length(Source) - 1)]) || Y <- lists:seq(1, length(Source))]),
     NewScore=levenshtein_distance_source(Source, Target, Score, 1, 1),
@@ -41,3 +47,35 @@ compare(X,Y)->
 	    0;
 	false -> 1
     end.
+
+-ifdef(TEST).
+
+distance_test_template(Source, Target, ExpectedScore)->
+    Score = levenshtein:levenshtein_distance(Source, Target),
+    ?assert(Score  =:= ExpectedScore).
+
+
+distance_001_test() ->
+    Source = "Levenshtein distance is named after the Russian scientist Vladimir Levenshtein, who devised the algorithm in 1965.",
+    Target = "distance  Russian  Levenshtein is named after the Russian scientist Vladimir, who devised the Levenshtein algorithm in 1965.",
+    distance_test_template(Source, Target, 48).
+
+
+distance_002_test() ->
+    Source = "He graduated from the Department of Mathematics and Mechanics of Moscow State University in 1958 and worked at the Keldysh Institute of Applied Mathematics in Moscow ever since. He was a fellow of the IEEE Information Theory Society.",
+    Target = "He received the IEEE Richard W. Hamming Medal in 2006, for \"contributions to the theory of error-correcting codes and information theory, including the Levenshtein distance\".",
+    distance_test_template(Source, Target, 173).
+
+
+distance_003_test() ->
+    Source = "",
+    Target = "",
+    distance_test_template(Source, Target, 0).
+
+
+distance_004_test() ->
+    Source = "igb",
+    Target = "rgb",
+    distance_test_template(Source, Target, 1).
+
+-endif.
